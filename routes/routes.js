61cb803a2a0540f9001953ce529666983dc9514b
Nick Corcoran <nicholascorcoran490@gmail.com>
2020-03-17 01:37:28 -0700

    s

--- routes/routes.js
+++ routes/routes.js
@@ -7,28 +7,28 @@ const lcontroller = require("../controllers/LoginController.js")
 
 
 
-router.get("https://comp-4711-lab6.herokuapp.com/home", (req, res) => {
+router.get("/home", (req, res) => {
     controller.getAllData(req,res);
 
 })
 
 // called when request is made to add a new artist
-router.post("https://comp-4711-lab6.herokuapp.com/new", (req, res) => {
+router.post("/new", (req, res) => {
     controller.addNewArtist(req,res);
 })
 
 //called when a request is made to delete an artist
-router.get("https://comp-4711-lab6.herokuapp.com/delete", (req, res) => {
+router.get("/delete", (req, res) => {
     controller.deleteAnArtist(req, res);
 })
 
 
-router.get("https://comp-4711-lab6.herokuapp.com/search", (req,res) => {
+router.get("/search", (req,res) => {
     controller.search(req,res)
 })
 
 
-router.get("https://comp-4711-lab6.herokuapp.com/login", (req,res) => {
+router.get("/login", (req,res) => {
     lcontroller.login(req,res)
 })
 
